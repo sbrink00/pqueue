55575f09e81794b7374b40cc5bab6759f2259525
sbrink00 <sbrink00@stuy.edu>
2019-10-08 10:16:24 -0400

    almost done with queuetest

--- queue_test.py
+++ queue_test.py
@@ -1,13 +1,13 @@
-from pqueue import Pqueue
+ from pqueue import Pqueue
 import sys
 
 def newCMP(a, b):
   if a == b: return 0
   if len(a) < len(b): return -1
   if len(b) < len(a): return 1
-  a = a.upper()
-  b = b.upper()
-  if a < b: return -1
+  c = a.upper()
+  d = b.upper()
+  if c < d: return -1
   else: return 1
 
 def getAryCommands(filename):
@@ -20,8 +20,16 @@ def executeCommands(lines):
   pq = Pqueue(newCMP)
   for line in lines:
     ary = line.split(" ")
-    
+    if ary[0][:2] = "po": pq.pop()
+    elif ary[0][:2] = "pu": pq.push(ary[1])
+    else: return pq.getOrderedList()
 
-
+def generateFile(filename, list):
+  file = open(filename, "w"):
+  string = ""
+  for str in list:
+    string += str + ","
+  string = string[:-1]
+  file.write(string)
 
 print(getAryCommands("test.blah"))
